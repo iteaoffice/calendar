9164a0820f1f6dfda84ee1fba82d29d3c2628ef3
Johan van der Heide <info@japaveh.nl>
2015-09-01 16:31:59 +0200

    Update of some small issues

--- src/Calendar/Entity/EntityAbstract.php
+++ src/Calendar/Entity/EntityAbstract.php
@@ -10,7 +10,6 @@
 
 namespace Calendar\Entity;
 
-use Project\Entity\EntityInterface;
 use Zend\InputFilter\InputFilter;
 use Zend\InputFilter\InputFilterAwareInterface;
 
@@ -33,7 +32,7 @@ abstract class EntityAbstract implements EntityInterface, InputFilterAwareInterf
      */
     public function has($prop)
     {
-        $getter = 'get'.ucfirst($prop);
+        $getter = 'get' . ucfirst($prop);
         if (method_exists($this, $getter)) {
             if ('s' === substr($prop, 0, -1) && is_array($this->$getter())) {
                 return true;
@@ -55,25 +54,25 @@ abstract class EntityAbstract implements EntityInterface, InputFilterAwareInterf
                 return implode('', array_slice(explode('\\', get_class($this)), -1));
             case 'dashed_entity_name':
                 $dash = function ($m) {
-                    return '-'.strtolower($m[1]);
+                    return '-' . strtolower($m[1]);
                 };
 
                 return preg_replace_callback('/([A-Z])/', $dash, lcfirst($this->get('entity_name')));
             case 'underscore_entity_name':
                 $underscore = function ($m) {
-                    return '_'.strtolower($m[1]);
+                    return '_' . strtolower($m[1]);
                 };
 
                 return preg_replace_callback('/([A-Z])/', $underscore, lcfirst($this->get('entity_name')));
             case 'underscore_full_entity_name':
                 $underscore = function ($m) {
-                    return '_'.strtolower($m[1]);
+                    return '_' . strtolower($m[1]);
                 };
 
                 return preg_replace_callback(
                     '/([A-Z])/',
                     $underscore,
-                    lcfirst(str_replace('\\', '', __NAMESPACE__).$this->get('entity_name'))
+                    lcfirst(str_replace('\\', '', __NAMESPACE__) . $this->get('entity_name'))
                 );
         }
     }
