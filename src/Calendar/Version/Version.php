d8e3aea24a000554d37667a0c160a970d555c81f
Johan van der Heide <info@japaveh.nl>
2016-03-17 12:46:40 +0100

    Release of 2.0

--- src/Calendar/Version/Version.php
+++ src/Calendar/Version/Version.php
@@ -21,7 +21,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion().
      */
-    const VERSION = '2.0.0-dev';
+    const VERSION = '2.0.0';
     /**
      * Github Service Identifier for version information is retrieved from.
      */
